52eabd7dc5a9f4043d8b7a15552a3f5509eb6d54
Houbler <101861967+Houbler@users.noreply.github.com>
2023-07-12 14:28:49 +0300

    Physical database 2.0

--- Chapter 2.2 SQL_beginner/DDL/DDL.sql
+++ Chapter 2.2 SQL_beginner/DDL/DDL.sql
@@ -4,25 +4,28 @@ CREATE TABLE IF NOT EXISTS public.publisher (
     city VARCHAR(255) NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS public.authors (
-    authors_id SERIAL PRIMARY KEY,
-    main_author VARCHAR(255) NOT NULL,
-    coauthor1 VARCHAR(255),
-    coauthor2 VARCHAR(255),
-    coauthor3 VARCHAR(255)
+CREATE TABLE IF NOT EXISTS public.author (
+    author_id SERIAL PRIMARY KEY,
+    author_name VARCHAR(255) NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS public.books (
     book_code SERIAL PRIMARY KEY,
     publisher INT NOT NULL,
-  	author INT NOT NULL,
     FOREIGN KEY (publisher) REFERENCES publisher (publisher_id) ON DELETE SET NULL,
-    FOREIGN KEY (author) REFERENCES authors (authors_id) ON DELETE SET NULL,
     title VARCHAR(255) NOT NULL,
     publication_date DATE,
     pages INTEGER,
     cost DECIMAL,
     amount INTEGER NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS public.authors_books (
+    author_book_id SERIAL PRIMARY KEY,
+    author_id INT NOT NULL,
+    FOREIGN KEY (author_id) REFERENCES author (author_id) ON DELETE SET NULL,
+    book_id INT NOT NULL,
+    FOREIGN KEY (book_id) REFERENCES books (book_code) ON DELETE SET NULL
 );
 
 CREATE TABLE IF NOT EXISTS public.reader (
