81a40577185641ce22cfc305116a5a801576e612
Houbler <101861967+Houbler@users.noreply.github.com>
2023-07-20 23:16:57 +0300

    DWH and sql, 1.5

--- Chapter 2.5 DWH/SQL_Script.sql
+++ Chapter 2.5 DWH/SQL_Script.sql
@@ -1,8 +1,8 @@
-WITH ALL_shops AS (SELECT * FROM shop_dns
+WITH all_shops AS (SELECT * FROM shop_dns
 		UNION ALL SELECT * FROM shop_mvideo
 		UNION ALL SELECT * FROM shop_sitilink)
 SELECT ash.shop_id, pr.product_name, SUM(sales_cnt) AS sales_fact, p.plan_cnt AS sales_plan, 
-		CAST(SUM (sales_cnt) AS decimal) / CAST(p.plan_cnt AS decimal) AS sales_attitude,
+		CAST(SUM (sales_cnt) AS DECIMAL) / CAST(p.plan_cnt AS DECIMAL) AS sales_attitude,
 		SUM(pr.price * sales_cnt) AS income_fact, (p.plan_cnt * pr.price) AS income_plan,
 		SUM(pr.price * sales_cnt) / (p.plan_cnt * pr.price) AS income_attitude FROM all_shops ash
 JOIN plan p ON ash.shop_id = p.shop_id AND ash.product_id = p.product_id
