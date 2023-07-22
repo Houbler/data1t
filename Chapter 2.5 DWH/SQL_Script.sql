2b6bce38582768fa10c0b40462f022909872ebbc
Houbler <101861967+Houbler@users.noreply.github.com>
2023-07-22 21:10:27 +0300

    DWH, updated, 2

--- Chapter 2.5 DWH/SQL_Script.sql
+++ Chapter 2.5 DWH/SQL_Script.sql
@@ -1,11 +1,14 @@
-WITH all_shops AS (SELECT * FROM shop_dns
-		UNION ALL SELECT * FROM shop_mvideo
-		UNION ALL SELECT * FROM shop_sitilink)
-SELECT ash.shop_id, pr.product_name, SUM(sales_cnt) AS sales_fact, p.plan_cnt AS sales_plan, 
-		CAST(SUM (sales_cnt) AS DECIMAL) / CAST(p.plan_cnt AS DECIMAL) AS sales_attitude,
-		SUM(pr.price * sales_cnt) AS income_fact, (p.plan_cnt * pr.price) AS income_plan,
-		SUM(pr.price * sales_cnt) / (p.plan_cnt * pr.price) AS income_attitude FROM all_shops ash
-JOIN plan p ON ash.shop_id = p.shop_id AND ash.product_id = p.product_id
-JOIN products pr ON ash.product_id = pr.product_id 
-GROUP BY ash.shop_id, pr.product_name, p.plan_cnt, pr.price
-ORDER BY shop_id, pr.product_name
+with all_shops as (select * from shop_dns
+		union all select * from shop_mvideo
+		union all select * from shop_sitilink)
+select DATE_PART('month', ash.sales_date) as month_sales, sn.shop_name, pr.product_name, sum(sales_cnt) as sales_fact, p.plan_cnt as sales_plan, 
+		cast(sum (sales_cnt) as decimal)/cast(p.plan_cnt as decimal) as sales_attitude,
+		sum(pr.price * sales_cnt) as income_fact, (p.plan_cnt * pr.price) as income_plan,
+		sum(pr.price * sales_cnt)/(p.plan_cnt * pr.price) as income_attitude 
+from all_shops ash
+join plan p on ash.shop_id = p.shop_id and ash.product_id = p.product_id
+join products pr on ash.product_id = pr.product_id 
+join shop_names sn on ash.shop_id = sn.shop_id  
+WHERE DATE_PART('month',  ash.sales_date) = DATE_PART('month', p.plan_date)
+group by sn.shop_name, pr.product_name, p.plan_cnt, pr.price, DATE_PART('month', ash.sales_date), DATE_PART('month', p.plan_date)
+order by month_sales, sn.shop_name, pr.product_name	
